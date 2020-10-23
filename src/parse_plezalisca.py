4e27bd4a07c5515c89291c5c254bfb0643c69506
urh <urh.primozic@gmail.com>
2020-10-23 23:14:41 +0200

    skripta in podatki za parsanje o balvaniščij

--- src/parse_plezalisca.py
+++ src/parse_plezalisca.py
@@ -34,3 +34,30 @@ def strik_v_json():
         }
         seznam_plezalisc.append(slovar)
     orodja.zapisi_json(seznam_plezalisc, os.path.join(mapa_s_podatki, 'plezalisca.json'))
+
+def balvani_v_json():
+    '''
+    Iz html-ja v plezalisca_balvani.html pridobi podatke o plezaliscih (ime, regija?, url) in jih zapise v JSON
+    '''
+    # passed
+    vzorec = (
+        r'<td class="col-name"'
+        r'.*class="name-link" data.*?<a href='
+        r'"(?P<url>.+?)"' # dobi url plezalisca
+        r'.data.*?>\n\s+(.+?/)?' #zignorira? potencialno regijo (Osp/Mišja Peč)
+        r'(?P<ime>.*)\n' # ime plezališča
+        r'\s*.*\n[\S\s\n]*?</a></span></p></td>\s?<td.*?>\n\s*(?P<stevilo_vzponov>.*)'
+    )
+    with open(datoteka_balvani, 'r') as txt:
+        besedilo = txt.read()
+    
+    seznam_plezalisc = []
+    for zadetek in re.finditer(pattern=vzorec, string=besedilo):
+        slovar_skupine = zadetek.groupdict()
+        slovar = {
+            'ime' : slovar_skupine.get('ime'),
+            'url' : 'https://www.8a.nu' + slovar_skupine.get('url')[:-6], # hočem cel url, ampak ne med routes
+            'stevilo_vzponov' : slovar_skupine.get('stevilo_vzponov')
+        }
+        seznam_plezalisc.append(slovar)
+    orodja.zapisi_json(seznam_plezalisc, os.path.join(mapa_s_podatki, 'balvani.json'))
